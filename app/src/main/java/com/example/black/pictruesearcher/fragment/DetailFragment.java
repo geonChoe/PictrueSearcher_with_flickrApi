c4433ba02d8da10ccee3453ec63ae9b3d884e6b9
geonChoe <chlrjsid101@naver.com>
2018-11-03 22:52:59 +0900

    바보같았다... 제대로 테스트 안하고 바로 올려버렸네...

--- app/src/main/java/com/example/black/pictruesearcher/fragment/DetailFragment.java
+++ app/src/main/java/com/example/black/pictruesearcher/fragment/DetailFragment.java
@@ -13,6 +13,7 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -48,6 +49,7 @@ import java.util.Date;
 import java.util.Locale;
 
 public class DetailFragment extends Fragment {
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 2051;
 
     private SearchItem searchItem;
     private ImageView imageView;
@@ -64,25 +66,44 @@ public class DetailFragment extends Fragment {
         button = view.findViewById(R.id.download);
 
         Glide.with(this).load(searchItem.getUrl()).thumbnail(0.5f).into(imageView);
-        
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "사진 저장 시작", Toast.LENGTH_LONG).show();
                 if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-                Thread thread = new BackgroundThread();
-                thread.start();
-                } else{
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),Manifest.permission.WRITE_EXTERNAL_STORAGE)){
-                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
-                    } else{
-                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
-                    }
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+                    Toast.makeText(getActivity(), "사진 저장 시작", Toast.LENGTH_LONG).show();
+                    Thread thread = new BackgroundThread();
+                    thread.start();
                 }
             }
         });
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+
+            } else {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                }
+            }
+        }
     }
 
     private class BackgroundThread extends Thread {
